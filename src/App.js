ef61bb36e7d79b3647f4f3321041be3b7a7004fb
NathanielDaniels <26263599+NathanielDaniels@users.noreply.github.com>
2021-01-12 10:20:06 -0600

    Everything added for basic functionality, working correctly.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,50 @@
 import React, {useState, useEffect} from 'react'
-import './App.css';
 
 function App() {
+  const [text, setText] = useState("")
+  const [timeRemaining, setTimeRemaining] = useState(2)
+  const [isTimeRunning, setIsTimeRunning] = useState(false)
+  const [wordCount, setWordCount] = useState('')
+
+  function handleChange(e) {
+    const {value} = e.target
+    setText(value)
+  }
+
+  function calculateWordCount(text) {
+    const wordArray = text.trim().split(' ')
+    return wordArray.filter((word) => word !== '').length
+  }
+
+  useEffect(() => {
+    if (isTimeRunning && timeRemaining > 0) {
+      setTimeout(() => {
+        setTimeRemaining(prevTime => prevTime - 1)
+      }, 1000)
+    } else if (timeRemaining === 0) {
+      setIsTimeRunning(false)
+      setTimeRemaining("Times Up!")
+      setWordCount(calculateWordCount(text))
+    }
+  }, [timeRemaining, isTimeRunning])
+
+
   return (
-    <>
-      <h1>Working</h1>
-    </>
+    <div>
+      <h1>How fast do you type?</h1>
+      <textarea
+          onChange={handleChange}
+          value={text}
+      />
+      <h4>Time remaining: {timeRemaining}</h4>
+      <button 
+        onClick={() => {
+          setIsTimeRunning(true)
+        }}
+        >Start
+      </button>
+      <h1>Word count: {wordCount}</h1>
+    </div>
   );
 }
 
