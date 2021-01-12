834e6b5615063be3931a609bf1c1abb64e5f70c8
NathanielDaniels <26263599+NathanielDaniels@users.noreply.github.com>
2021-01-12 17:23:55 -0600

    added custom hook to store business logic

--- src/App.js
+++ src/App.js
@@ -1,48 +1,8 @@
-import React, {useState, useEffect, useRef} from 'react'
+import React from 'react'
+import useWordGame from "./hooks/useWordGame"
 
 function App() {
-  const startTime = 10
-  const [text, setText] = useState("")
-  const [timeRemaining, setTimeRemaining] = useState(startTime)
-  const [isTimeRunning, setIsTimeRunning] = useState(false)
-  const [wordCount, setWordCount] = useState('')
-  const textBoxRef = useRef(null)
-
-  function handleChange(e) {
-    const {value} = e.target
-    setText(value)
-  }
-
-  function calculateWordCount(text) {
-    const wordArray = text.trim().split(' ')
-    return wordArray.filter((word) => word !== '' && word !== ',' && word !== '.' && word !== '/' && word !== '?' && word !== '>' && word !== '<').length
-  }
-
-  function endGame() {
-    setIsTimeRunning(false)
-    setTimeRemaining("Times Up!")
-    setWordCount(calculateWordCount(text))
-  }
-
-  function startGame() {
-    setIsTimeRunning(true)
-    setText("")
-    setTimeRemaining(startTime)
-    setWordCount('-')
-    textBoxRef.current.disabled = false
-    textBoxRef.current.focus()
-  }
-
-  useEffect(() => {
-    if (isTimeRunning && timeRemaining > 0) {
-      setTimeout(() => {
-        setTimeRemaining(prevTime => prevTime - 1)
-      }, 1000)
-    } else if (timeRemaining === 0) {
-      endGame()
-    }
-  }, [timeRemaining, isTimeRunning])
-
+  const {text, handleChange ,timeRemaining, isTimeRunning, wordCount, textBoxRef, startGame} = useWordGame()
 
   return (
     <div>
---
+++ src/hooks/useWordGame.js
@@ -0,0 +1,50 @@
+import {useState, useEffect, useRef}from 'react'
+
+function useWordGame() {
+  const startTime = 10
+  const [text, setText] = useState("")
+  const [timeRemaining, setTimeRemaining] = useState(startTime)
+  const [isTimeRunning, setIsTimeRunning] = useState(false)
+  const [wordCount, setWordCount] = useState('')
+  const textBoxRef = useRef(null)
+
+  function handleChange(e) {
+    const {value} = e.target
+    setText(value)
+  }
+
+  function calculateWordCount(text) {
+    const wordArray = text.trim().split(' ')
+    return wordArray.filter((word) => word !== '' && word !== ',' && word !== '.' && word !== '/' && word !== '?' && word !== '>' && word !== '<').length
+  }
+
+  function endGame() {
+    setIsTimeRunning(false)
+    setTimeRemaining("Times Up!")
+    setWordCount(calculateWordCount(text))
+  }
+
+  function startGame() {
+    setIsTimeRunning(true)
+    setText("")
+    setTimeRemaining(startTime)
+    setWordCount('-')
+    textBoxRef.current.disabled = false
+    textBoxRef.current.focus()
+  }
+
+  useEffect(() => {
+    if (isTimeRunning && timeRemaining > 0) {
+      setTimeout(() => {
+        setTimeRemaining(prevTime => prevTime - 1)
+      }, 1000)
+    } else if (timeRemaining === 0) {
+      endGame()
+    }
+  }, [timeRemaining, isTimeRunning])
+
+  return {text, handleChange ,timeRemaining, isTimeRunning, wordCount, textBoxRef, startGame}
+
+}
+
+export default useWordGame
