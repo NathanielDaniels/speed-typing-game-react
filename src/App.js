52cf0d5443d2852a0f1e274816250506b21e54a7
NathanielDaniels <26263599+NathanielDaniels@users.noreply.github.com>
2021-01-13 09:31:12 -0600

    adding navbar for routing + cleaning up CSS

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 import React from 'react'
 import useWordGame from "./hooks/useWordGame"
+import {Link} from "react-router-dom"
 
 function App() {
   const {text, handleChange ,timeRemaining, isTimeRunning, wordCount, textBoxRef, startGame} = useWordGame(10)
 
   return (
     <div>
-      <h1>How fast do you type?</h1>
-      <textarea
-        type="text"
-        ref={textBoxRef}
-        onChange={handleChange}
-        value={text}
-        disabled={!isTimeRunning}
-      />
-      <h4>Time remaining: {timeRemaining}</h4>
-      <button 
-        onClick={() => startGame()}
-        disabled={isTimeRunning}
-        >Start
-      </button>
-      <h1>Word count: {wordCount}</h1>
+      <div className="navbar">
+        <Link>About</Link>
+        <Link>Contact</Link>
+      </div>
+      <div>
+        <h1>How fast do you type?</h1>
+        <textarea
+          type="text"
+          ref={textBoxRef}
+          onChange={handleChange}
+          value={text}
+          disabled={!isTimeRunning}
+        />
+        <h4>Time remaining: {timeRemaining}</h4>
+        <button 
+          onClick={() => startGame()}
+          disabled={isTimeRunning}
+          >Start
+        </button>
+        <h1>Word count: {wordCount}</h1>
+      </div>
     </div>
   );
 }
