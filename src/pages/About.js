441bea93efc92d6c0b856ca55fe3e9eb45859331
NathanielDaniels <26263599+NathanielDaniels@users.noreply.github.com>
2021-01-14 17:08:23 -0600

    positioning each page

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,7 +5,7 @@ function About() {
   return (
     <>
       <div className="about-section">
-        <h1>About Speed Typing</h1>
+        <h1>How to play</h1>
         <div className="about-info">
           <p>This Speed Typing Game is a fun way to practice your typing skills.</p> 
           <p>Press the green "Start" button and type as many words as you can before the time is up. </p>
