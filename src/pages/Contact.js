f2d559207c30ae7864330a1595857433f7356626
NathanielDaniels <26263599+NathanielDaniels@users.noreply.github.com>
2021-01-15 09:18:25 -0600

    added fontAwesome Icons, positioned each page correctly.

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,15 +1,60 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTwitter } from '@fortawesome/free-brands-svg-icons'
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
 
-function Contact() {
-
+function Contact () {
   return (
     <>
-      <div className="contact-section">
-        <h1>Contact Page</h1>
-        <p>Welcome to the Contact Page.</p>
+      <div className='contact-section'>
+        <h1>Get in touch</h1>
+        <aside>
+          <ul className='social-media'>
+            <li>
+              <a
+                target='_blank'
+                title='Twitter'
+                rel='noreferrer noopener'
+                href='https://twitter.com/NathanDDaniels'
+              >
+                <div className='social-icon-container'>
+                  <h4>Twitter</h4>
+                  <FontAwesomeIcon className='social-icon' icon={faTwitter} />
+                </div>
+              </a>
+            </li>
+            <li>
+              <a
+                target='_blank'
+                title='linkedIn'
+                rel='noreferrer noopener'
+                href='https://www.linkedin.com/in/nathaniel-daniels-500740139/'
+              >
+                <div className='social-icon-container'>
+                  <h4>LinkedIn</h4>
+                  <FontAwesomeIcon className='social-icon' icon={faLinkedin} />
+                </div>
+              </a>
+            </li>
+            <li>
+              <a
+                target='_blank'
+                title='GitHub'
+                rel='noreferrer noopener'
+                href='https://github.com/NathanielDaniels'
+              >
+                <div className='social-icon-container'>
+                  <h4>Github</h4>
+                  <FontAwesomeIcon className='social-icon' icon={faGithub} />
+                </div>
+              </a>
+            </li>
+          </ul>
+        </aside>
       </div>
     </>
-  );
+  )
 }
 
-export default Contact;
+export default Contact
